1fd25189935977d54513d5566e2230bdd7df7a36
Kodjo Laurent Egbakou <egbakoukodjolaurent@gmail.com>
2021-03-31 21:27:28 +0200

    Add project files.

---
+++ remote-ftp-server/Constants/FTPServerResource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteFTPServer.Constants
+{
+    public static class FTPServerResource
+    {
+        public static string HOST = "demo.wftpserver.com";
+        public static string FOLDER = "/download";
+        public static string USERNAME = "demo";
+        public static string PASSWORD = "demo";
+
+
+    }
+}
---
+++ remote-ftp-server/Program.cs
@@ -0,0 +1,55 @@
+﻿using ByteSizeLib;
+using FluentFTP;
+using RemoteFTPServer.Constants;
+using System;
+using System.Threading.Tasks;
+using static RemoteFTPServer.Constants.FTPServerResource;
+
+namespace RemoteFTPServer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await ConnectFTPSAsync();
+        }
+
+
+
+        public static async Task ConnectFTPSAsync()
+        {
+            var conn = new FtpClient(HOST, USERNAME, PASSWORD);
+            await conn.ConnectAsync();
+
+            // get the list of files and directories in the "/download" folder
+            foreach (var item in await conn.GetListingAsync(FTPServerResource.FOLDER))
+            {
+
+                switch (item.Type)
+                {
+
+                    case FtpFileSystemObjectType.Directory:
+
+                        Console.WriteLine("Directory !  " + item.FullName);
+                        Console.WriteLine("Modified date:  " + await conn.GetModifiedTimeAsync(item.FullName));
+
+                        break;
+
+                    case FtpFileSystemObjectType.File:
+
+                        Console.WriteLine("File!  " + item.FullName);
+                        Console.WriteLine("File size:  " +  ByteSize.FromBytes(item.Size).MegaBytes.ToString() + " Mo");
+                        Console.WriteLine("Modified date:  " + await conn.GetModifiedTimeAsync(item.FullName));
+
+                        break;
+
+                    case FtpFileSystemObjectType.Link:
+                        break;
+                }
+            }
+        }
+    }
+
+
+   
+}
